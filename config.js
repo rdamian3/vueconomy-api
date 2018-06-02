c84664420ec24880f049be5c70c0ae8ce4bfe6ae
Damián Acevedo <rdamian3@gmail.com>
2018-06-02 18:25:11 -0300

    changed db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/vueconomy',
   SECRET_TOKEN: 'miclavedetokens',
 };
