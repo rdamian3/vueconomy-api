2f1bc50aaf7e150928973fe14a2470fa402406a1
Damián <rdamian3@gmail.com>
2019-02-28 20:37:18 -0300

    deploy

--- controllers/user.js
+++ controllers/user.js
@@ -8,14 +8,16 @@ function signUp(req, res) {
   const user = new User({
     email: req.body.email,
     displayName: req.body.displayName,
-    password: req.body.password,
+    password: req.body.password
   });
 
-  if (Utils.checkUserNotNull(user)) {
+  if (user.email != '' || user.email != null) {
     user.save(err => {
       if (err) {
         if (err.code == 11000) {
-          return res.status(500).send("Error al crear el usuario: Usuario duplicado");
+          return res
+            .status(500)
+            .send('Error al crear el usuario: Usuario duplicado');
         }
         return res.status(500).send({
           message: `Error al crear el usuario: ${err}`
@@ -28,7 +30,7 @@ function signUp(req, res) {
     });
   } else {
     res.status(500).send({
-      message: "Error al crear el usuario"
+      message: 'Error al crear el usuario'
     });
   }
 }
@@ -36,33 +38,37 @@ function signUp(req, res) {
 function signIn(req, res) {
   const email = req.body.email;
   const pswd = req.body.password;
-  if (email == "" || pswd == "") {
+  if (email == '' || pswd == '') {
     res.status(401).send({
-      message: "Falta usuario o contraseña..."
-    })
-  } else {
-    User.find({
-      email: req.body.email
-    }, (err, user) => {
-      if (err) return res.status(500).send({
-        message: err
-      });
-      if (!user || user.length == 0) {
-        return res.status(404).send({
-          message: 'No existe el usuario'
-        });
-      } else {
-        req.user = user;
-        res.status(200).send({
-          message: 'Te has logueado correctamente',
-          token: service.createToken(user),
-        });
-      }
+      message: 'Falta usuario o contraseña...'
     });
+  } else {
+    User.find(
+      {
+        email: req.body.email
+      },
+      (err, user) => {
+        if (err)
+          return res.status(500).send({
+            message: err
+          });
+        if (!user || user.length == 0) {
+          return res.status(404).send({
+            message: 'No existe el usuario'
+          });
+        } else {
+          req.user = user;
+          res.status(200).send({
+            message: 'Te has logueado correctamente',
+            token: service.createToken(user)
+          });
+        }
+      }
+    );
   }
 }
 
 module.exports = {
   signUp,
-  signIn,
+  signIn
 };
