5f376a36a93fd9321c384e8c3a477f53af26d5fd
Damián <rdamian3@gmail.com>
2019-03-12 00:09:12 -0300

    Errors WIP

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config');
 
-mongoose.connect(config.db, (err, res) => {
-  if (err) {
-    return console.log(`Error al conectar a la base de datos: ${err}`);
-  }
-  console.log('Conexión a la base de datos establecida...');
+mongoose.connect(
+  config.db,
+  { useCreateIndex: true, useNewUrlParser: true },
+  (err, res) => {
+    if (err) {
+      return console.log(`Error al conectar a la base de datos: ${err}`);
+    }
+    console.log('Conexión a la base de datos establecida...');
 
-  app.listen(config.port, () => {
-    console.log(`API REST corriendo en http://localhost:${config.port}`);
-  });
-});
+    app.listen(config.port, () => {
+      console.log(`API REST corriendo en http://localhost:${config.port}`);
+    });
+  }
+);
