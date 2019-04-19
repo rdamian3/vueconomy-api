3f90b0d657135d29623b04c64ca3c9bcc93c41a9
Damián <rdamian3@gmail.com>
2019-04-18 21:04:03 -0300

    Fixed indentation

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const hbs = require('express-handlebars')
-const app = express()
-const api = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
+const app = express();
+const api = require('./routes');
 const morgan = require('morgan');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-})
+});
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
-app.use(bodyParser.json())
-app.engine('.hbs', hbs({
-  defaultLayout: 'default',
-  extname: '.hbs'
-}))
-app.set('view engine', '.hbs')
+app.use(bodyParser.json());
+app.engine(
+  '.hbs',
+  hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+  })
+);
+app.set('view engine', '.hbs');
 
-app.use('/api', api)
+app.use('/api', api);
 app.get('/login', (req, res) => {
-  res.render('login')
-})
+  res.render('login');
+});
 app.get('/', (req, res) => {
-  res.render('login')
-})
+  res.render('login');
+});
 
-module.exports = app
+module.exports = app;
