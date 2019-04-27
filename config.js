5b2ed9d1ae2d8052ecdb8928055e22cc818341ac
Damián <rdamian3@gmail.com>
2019-04-27 11:18:34 -0300

    Changed DB config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb://redes-test:D33luxroW6k6Q1avC6a0HC25cHbFFYhHJwxv9W0tKVLr7smeUZ1ZI9vA8VpNtefIXYF8Bg5wpmQXQgm19uxRdQ==@redes-test.documents.azure.com:10255/?ssl=true',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/vueconomy',
   SECRET_TOKEN: 'miclavedetokens',
   emailUser: 'rdamian3dev@gmail.com',
   emailPassword: '!Dd302010d'
