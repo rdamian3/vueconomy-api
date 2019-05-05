1940a7ba24b227ae406e041f01491071d29607fe
Damián <rdamian3@gmail.com>
2019-05-05 16:35:32 -0300

    Trying cors

--- app.js
+++ app.js
@@ -3,20 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const hbs = require('express-handlebars');
+const cors = require('cors');
 const app = express();
 const api = require('./routes');
 const morgan = require('morgan');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors());
 
 app.use(morgan('dev'));
 
