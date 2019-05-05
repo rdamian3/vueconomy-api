350183b67c1b4fb2ac6f0a1be237ee7b1071aa70
Damian <rdamian3@gmail.com>
2019-05-05 00:54:46 -0300

    Allowing cors credentials

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
   );
+  res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
