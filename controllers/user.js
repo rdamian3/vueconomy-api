8e02425fd46d989c755ea54df1d7084925ea0678
Damian Acevedo <damian.acevedo@globant.com>
2019-07-25 15:25:10 -0300

    Changed user s3 config

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const User = require('../models/user');
-const service = require('../services');
-const nodemailer = require('nodemailer');
-const config = require('../config');
-const s3 = require('../services/s3');
+const User = require("../models/user");
+const service = require("../services");
+const nodemailer = require("nodemailer");
+const config = require("../config");
+const s3 = require("../services/s3");
 
 function signUp(req, res) {
-  if (req.body.email !== '' || req.body.email !== null) {
+  if (req.body.email !== "" || req.body.email !== null) {
     if (req.body.email.length < 50) {
       s3.createUserBucket(req.body.email).then(bucket => {
         const user = new User({
@@ -19,7 +19,9 @@ function signUp(req, res) {
         user.save(err => {
           if (err) {
             if (err.code === 11000) {
-              return res.status(500).send('Error creating user: duplicated user');
+              return res
+                .status(500)
+                .send("Error creating user: duplicated user");
             }
             return res.status(500).send({
               message: `Error creating user: ${err}`
@@ -33,12 +35,12 @@ function signUp(req, res) {
       });
     } else {
       res.status(500).send({
-        message: 'Too many characters for email: 50+'
+        message: "Too many characters for email: 50+"
       });
     }
   } else {
     res.status(500).send({
-      message: 'Error creating user'
+      message: "Error creating user"
     });
   }
 }
@@ -46,8 +48,8 @@ function signUp(req, res) {
 function signIn(req, res) {
   const email = req.body.email;
   const pswd = req.body.password;
-  if (email === '' || pswd === '') {
-    res.status(401).send({ message: 'User or password missing' });
+  if (email === "" || pswd === "") {
+    res.status(401).send({ message: "User or password missing" });
   } else {
     User.findOne(
       {
@@ -60,19 +62,19 @@ function signIn(req, res) {
           });
         if (!user || user.length === 0) {
           return res.status(404).send({
-            message: 'User doesnt exists'
+            message: "User doesnt exists"
           });
         }
         user.comparePassword(pswd, user.password, (err, areEqual) => {
           if (areEqual) {
             res.status(200).send({
-              message: 'Logged in successfuly',
+              message: "Logged in successfuly",
               token: service.createToken(user),
               user
             });
           } else {
             return res.status(403).send({
-              message: 'Invalid user or password'
+              message: "Invalid user or password"
             });
           }
         });
@@ -82,20 +84,22 @@ function signIn(req, res) {
 }
 
 function deleteUser(req, res) {
-  if (req.params.userId !== '') {
+  if (req.params.userId !== "") {
     User.findById(req.params.userId, (err, user) => {
       if (err) {
         res.status(500).send({ message: `Error while deleting user: ${err}` });
       } else if (user !== null) {
         user.remove(err => {
           if (err) {
-            res.status(500).send({ message: `Error while deleting user: ${err}` });
+            res
+              .status(500)
+              .send({ message: `Error while deleting user: ${err}` });
           } else {
-            res.status(200).send({ message: 'User eliminated' });
+            res.status(200).send({ message: "User eliminated" });
           }
         });
       } else {
-        res.status(500).send({ message: 'User not found' });
+        res.status(500).send({ message: "User not found" });
       }
     });
   }
@@ -106,28 +110,37 @@ function updateUser(req, res) {
   const update = req.body;
 
   if (userId !== null) {
-    User.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
-      if (err) {
-        res.status(500).send({ message: `Error updating user: ${err}` });
-      } else {
-        res.status(200).send({ user: userUpdated });
+    User.findByIdAndUpdate(
+      userId,
+      update,
+      { new: true },
+      (err, userUpdated) => {
+        if (err) {
+          res.status(500).send({ message: `Error updating user: ${err}` });
+        } else {
+          res.status(200).send({ user: userUpdated });
+        }
       }
-    });
+    );
   }
 }
 
 function uploadData(req, res) {
-  s3.uploadToBucket(req, res).then(result => {
-    const userId = result.userId;
-    const avatar = result.file;
-    const newObj = { ...req.body, avatar, userId };
-    req.body = newObj;
-    updateUser(req, res);
-  });
+  s3.uploadToBucket(req, res)
+    .then(result => {
+      const userId = result.userId;
+      const avatar = result.file;
+      const newObj = { ...req.body, avatar, userId };
+      req.body = newObj;
+      updateUser(req, res);
+    })
+    .catch(e => {
+      res.status(500).send({ message: "No se pudo agregar" });
+    });
 }
 
 function reqResetPassword(req, res) {
-  if (req.body.email !== '' && req.body.email !== null) {
+  if (req.body.email !== "" && req.body.email !== null) {
     User.findOne(
       {
         email: req.body.email
@@ -139,11 +152,11 @@ function reqResetPassword(req, res) {
           });
         if (!user || user.length === 0) {
           return res.status(404).send({
-            message: 'User doesnt exists'
+            message: "User doesnt exists"
           });
         }
         const transporter = nodemailer.createTransport({
-          service: 'gmail',
+          service: "gmail",
           auth: {
             user: config.emailUser,
             pass: config.emailPassword
@@ -151,9 +164,9 @@ function reqResetPassword(req, res) {
         });
 
         const mailOptions = {
-          from: 'rdamian3dev@gmail.com',
+          from: "rdamian3dev@gmail.com",
           to: user.email,
-          subject: 'Reset your password',
+          subject: "Reset your password",
           html: `<strong>Password Reset</strong><br>
             <p>Hi ${user.displayName} </p><br>
               <p>Please use this link to reset your password</p><br>
@@ -164,21 +177,21 @@ function reqResetPassword(req, res) {
           if (error) {
             console.log(error);
           } else {
-            console.log('Email sent: ' + info.response);
-            res.status(200).send({ message: 'Email sent' });
+            console.log("Email sent: " + info.response);
+            res.status(200).send({ message: "Email sent" });
           }
         });
       }
     );
   } else {
-    res.status(400).send({ message: 'No email provided' });
+    res.status(400).send({ message: "No email provided" });
   }
 }
 
 function replacePassword(req, res) {
   const userEmail = req.body.email;
 
-  if (req.body.email !== '') {
+  if (req.body.email !== "") {
     User.findOneAndUpdate(
       { email: userEmail },
       { $set: req.body },
@@ -196,9 +209,9 @@ function replacePassword(req, res) {
 
 function checkAuth(req, res) {
   if (req.user.status === 200) {
-    res.status(200).send({ message: 'auth is valid' });
+    res.status(200).send({ message: "auth is valid" });
   } else {
-    res.status(500).send({ message: 'You have no authorization' });
+    res.status(500).send({ message: "You have no authorization" });
   }
 }
 
