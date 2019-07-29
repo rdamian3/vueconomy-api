398519f9327256e13ad84777a58e6b8a31392d66
Damian Acevedo <damian.acevedo@globant.com>
2019-07-29 18:01:15 -0300

    added delete and update categories routes

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,31 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const movementCtrl = require('../controllers/movement');
-const categoryCtrl = require('../controllers/category');
-const userCtrl = require('../controllers/user');
-const auth = require('../middlewares/auth');
+const express = require("express");
+const movementCtrl = require("../controllers/movement");
+const categoryCtrl = require("../controllers/category");
+const userCtrl = require("../controllers/user");
+const auth = require("../middlewares/auth");
 const api = express.Router();
 
-api.get('/movement', auth, movementCtrl.getMovements);
-api.get('/movement/:movementId', movementCtrl.getMovement);
-api.post('/movement', auth, movementCtrl.saveMovement);
-api.put('/movement/:movementId', auth, movementCtrl.updateMovement);
-api.delete('/movement/:movementId', auth, movementCtrl.deleteMovement);
+api.delete("/movement/:movementId", auth, movementCtrl.deleteMovement);
+api.get("/movement", auth, movementCtrl.getMovements);
+api.get("/movement/:movementId", movementCtrl.getMovement);
+api.post("/movement", auth, movementCtrl.saveMovement);
+api.put("/movement/:movementId", auth, movementCtrl.updateMovement);
 
-api.post('/category', auth, categoryCtrl.addCategory);
-api.get('/category', auth, categoryCtrl.getCategories);
+api.delete("/category", auth, categoryCtrl.deleteCategory);
+api.get("/category", auth, categoryCtrl.getCategories);
+api.post("/category", auth, categoryCtrl.addCategory);
+api.post("/category", auth, categoryCtrl.updateCategory);
 
-api.post('/signup', userCtrl.signUp);
-api.post('/signin', userCtrl.signIn);
-api.delete('/deleteuser/:userId', auth, userCtrl.deleteUser);
-api.put('/updateuser', auth, userCtrl.updateUser);
-api.get('/hasauth', auth, userCtrl.checkAuth);
-api.post('/upload', auth, userCtrl.uploadData);
+api.delete("/deleteuser/:userId", auth, userCtrl.deleteUser);
+api.get("/hasauth", auth, userCtrl.checkAuth);
+api.post("/signin", userCtrl.signIn);
+api.post("/signup", userCtrl.signUp);
+api.post("/upload", auth, userCtrl.uploadData);
+api.put("/updateuser", auth, userCtrl.updateUser);
 
-api.post('/resetpassword', userCtrl.reqResetPassword);
-api.post('/replacepassword', userCtrl.replacePassword);
+api.post("/replacepassword", userCtrl.replacePassword);
+api.post("/resetpassword", userCtrl.reqResetPassword);
 
 module.exports = api;
